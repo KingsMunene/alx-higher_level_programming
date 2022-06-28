c9623d3021a87d0bc7889c6db32b7bdd9456aa2f
KingsMunene <munenekings2@gmail.com>
2022-06-28 09:45:36 +0000

    Fizz Buzz unction

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,5 +9,5 @@ def fizzbuzz():
         elif i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz ", end="")
         else:
-            print("{}".format(i), end="")
+            print("{}".format(i), end=" ")
 print()
