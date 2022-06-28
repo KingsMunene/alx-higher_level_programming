3edd1d05e765eddb674f03dc749587f693f1d12c
KingsMunene <munenekings2@gmail.com>
2022-06-28 05:05:07 +0000

    This code prints alphabets

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 i = 0
+# The range of lowcase alphabets to be looped through
 alphabetRange = range(97, 123)
 while i < 26:
+    # print the alphabet at a given number in the range
+    # use chr to convert the int to a character
     print("{}".format(chr(alphabetRange[i])), end="")
     i = i + 1
