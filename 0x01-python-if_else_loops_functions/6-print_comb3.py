8fff2e4b04dddfcb209c1d0e4c8218cfc92f1855
KingsMunene <munenekings2@gmail.com>
2022-06-28 08:06:57 +0000

    Prints combination of two digits

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+i = 0
+numberRange = range(0, 10)
+
+while i < 10:
+    for j in range(i + 1, 10):
+        if (not (numberRange[i] == 8 and j == 9)):
+            print("{}{}".format(numberRange[i], j), end=", ")
+        else:
+            print("{}{}".format(i,j))
+    i += 1
