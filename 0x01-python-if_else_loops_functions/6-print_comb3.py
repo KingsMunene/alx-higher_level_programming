df6bd2caf052ff0279b8478845f51045d0ef8ecb
KingsMunene <munenekings2@gmail.com>
2022-06-28 08:08:35 +0000

    Prints combination of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -7,5 +7,5 @@ while i < 10:
         if (not (numberRange[i] == 8 and j == 9)):
             print("{}{}".format(numberRange[i], j), end=", ")
         else:
-            print("{}{}".format(i,j))
+            print("{}{}".format(i, j))
     i += 1
