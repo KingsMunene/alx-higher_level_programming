73bd00454d05699c84081bffd6b68ef28569975a
KingsMunene <munenekings2@gmail.com>
2022-06-28 04:37:54 +0000

    outputs a number and its last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-# Convert number to positive extract the last digit convert back to negative
+    #Convert number to positive extract the last digit convert back to negative
     lastDigit = ((number * -1) % 10) * -1
 else:
     lastDigit = number % 10
@@ -12,5 +12,5 @@ if lastDigit > 5:
 elif lastDigit == 0:
     print("Last digit of {} is {} and is 0".format(number, lastDigit))
 elif lastDigit < 6 and lastDigit != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number,
-                                                                    lastDigit))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(
+        number, lastDigit))
